645e8dc4af2dcad31d9f1eb497812a8671fcb15e
ErrorX <felix@edenborgh.se>
2019-05-08 13:52:01 +0200

    update DAO

--- webservice/src/main/java/dashboard/DAOInterface.java
+++ webservice/src/main/java/dashboard/DAOInterface.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dashboard;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import com.mysql.jdbc.Connection;
+//import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Driver;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author Felix
- */
+
 public class DAOInterface {
+    public static void main(String[] args) throws ClassNotFoundException{
+        Class.forName("com.mysql.jdbc.Driver");
+        
+        try{
+            connectToDatabase();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Something whent wrong");
+        }
+        
+    }
+    
+    
+    public static void connectToDatabase() throws SQLException{
+        String username = ""; // database name mostly "root"
+        String password = ""; // database password
+        
+        
+        try(Connection con = (Connection) DriverManager.getConnection(" jdbc:MySQL://ip:port/databasename", username, password);){
+            if(con != null){
+                System.out.println("Connection succes!");
+            }
+        }
+        
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println("No Connection to database");
+        }
+        
+    }
+    
     
 }
