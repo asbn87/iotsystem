3965214dca56df9148034c8f60c65ba69997a6e1
emiljaregran <45233952+emiljaregran@users.noreply.github.com>
2019-05-09 19:02:30 +0200

    Adding program for esp8266-03 (ds18b20 temperature sensor)

---
+++ device/esp8266-03/config.h
@@ -0,0 +1,10 @@
+#define DEVICE_ID 3
+#define ONE_WIRE_PIN 5
+#define TEMPSENSOR_1 0
+
+const char* WIFI_SSID = "SSID";
+const char* WIFI_PASS = "PASSWORD";
+
+char websocketPath[] = "/webservice/websocket/realtime/3";
+char destinationHost[] = "IP-ADDRESS";
+const int destinationPort = 8080;
