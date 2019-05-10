e045f691f69684485660fa1704ce7fdcd32020e0
Andreas Bengtsson <asbn87@gmail.com>
2019-05-10 14:57:57 +0200

    Updated code for devices

--- device/esp8266-03/functions.h
+++ device/esp8266-03/functions.h
@@ -1,21 +1,60 @@
+// Package
+struct Package {
+  float temperature = 0.0;
+  char* description = "temperature";
+
+  String DateTime()
+  {
+    time_t now;
+    struct tm ts;
+    char buf[80];
+    
+    // Get current time
+    time(&now);
+
+    // Format time
+    ts = *localtime(&now);
+    ts.tm_hour += 2; // Local Summertime Western Europe +2
+    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", &ts);
+  
+    return (String)buf;
+  }
+  
+  void updateReadings() {
+    sensors.requestTemperatures();
+    temperature = sensors.getTempCByIndex(TEMPSENSOR_1);
+  }
+};
+struct Package data;
+
+void initSerial(int baud)
+{
+  Serial.begin(baud);
+  delay(2000);
+  Serial.println("--Serial communication initiated--");
+  Serial.println("Baud rate: "+ String(baud));
+}
+
 void initWifi()
 {
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.println("\n--Wifi communication initiated--\nPlease wait...");
+  Serial.println("Connecting to: "+(String)WIFI_SSID);
+  WiFi.mode(WIFI_STA);
   
-  while (WiFi.status() != WL_CONNECTED)
+  while(WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
-    Serial.print(".");
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    delay(10000);
   }
 
-  Serial.println("\nWiFi connected");  
-  Serial.println("IP address: ");
+  Serial.printf("\nConnected to Wifi Network %s \n", WIFI_SSID);
+  Serial.print("IP-adress: ");
   Serial.println(WiFi.localIP());
 }
 
 void websocketConnect(WiFiClient& wifiClient)
 {
-  if (wifiClient.connect(destinationHost, destinationPort))
+  if (wifiClient.connect(webserverHost, webserverPort))
   {
     Serial.println("WebSocket connected.");
   }
@@ -32,7 +71,7 @@ void websocketConnect(WiFiClient& wifiClient)
 void websocketHandshake(WiFiClient& wifiClient, WebSocketClient& websocketClient)
 {
   websocketClient.path = websocketPath;
-  websocketClient.host = destinationHost;
+  websocketClient.host = webserverHost;
   
   if (websocketClient.handshake(wifiClient))
   {
@@ -46,4 +85,16 @@ void websocketHandshake(WiFiClient& wifiClient, WebSocketClient& websocketClient
       // Hang on failure
     }  
   }
+}
+
+void createMessage(char* json) {
+  data.updateReadings();
+  StaticJsonBuffer<MESSAGE_MAX_LEN> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["dateTime"] = data.DateTime();
+  root["mac"] = DEVICE_ID;
+  root["description"] = data.description;
+  root["temperature"] = data.temperature;
+
+  root.printTo(json, MESSAGE_MAX_LEN);
 }
